606ee0fc9050781ffbdc78745e9828967871da1a
Grant Lemons <grantlemons@aol.com>
2025-12-15 14:07:09 -0700

    move comparison files after student code

--- run_student_code.sh
+++ run_student_code.sh
@@ -9,9 +9,10 @@ rm -f *.{vm,asm,tst}
 
 # copy test files over
 cp /autograder/grader/tests/${1}.{vm,tst} .
-cp /autograder/grader/tests/expected-outputs/${1}.cmp .
 
 # run student-submitted code (untrusted)
 runuser -u student -- ${EXECUTABLE} ${1}.vm
+
+cp -f /autograder/grader/tests/expected-outputs/${1}.cmp .
 
 popd >/dev/null
