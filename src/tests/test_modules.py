c69efa8eff4d3d3cb2ddd048d6b0949becfb2bec
Grant Lemons <grantlemons@aol.com>
2026-05-09 18:13:56 -0600

    only print if stdout or stderr is not empty

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -8,7 +8,10 @@ class TestBase(unittest.TestCase):
     def runStudentCode(self, name):
         try:
             process = subprocess.run(['./run_student_code.sh', f'{name}.vm'], check=True, text=True, capture_output=True, timeout=30)
-            print(f'{process.stdout.strip()}\n{process.stderr.strip()}'.strip())
+            if len(process.stdout.strip()) > 0:
+                print(process.stdout.strip())
+            if len(process.stderr.strip()) > 0:
+                print(process.stderr.strip())
         except subprocess.CalledProcessError as err:
             error_message = str(err.stderr).strip()
             raise AssertionError(f'Unable to run student code on {name}.vm: "{error_message}"\n{err.stdout}'.strip())
